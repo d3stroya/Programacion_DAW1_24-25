37e290e4b3179efa8aba071130feb03fb124895e
d3stroya <anabel.comino@gmail.com>
2024-11-27 20:15:58 +0100

    Tema 4 - ejercicio 25

---
+++ Tema04/src/tema04/ejercicios/Ejercicio25T4.java
@@ -0,0 +1,42 @@
+
+package tema04.ejercicios;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author d3stroya
+ */
+public class Ejercicio25T4 {
+    public static double potencia(int a, int b) {
+        return Math.pow(a, b);
+    }
+    
+    public static int pedirNumero() {
+        Scanner entrada = new Scanner(System.in);
+        return entrada.nextInt();
+    }
+    
+    public static void main(String[] args) {
+//        int base, exponente;
+//        double resultado;
+//        
+//        try {
+//            System.out.print("Escribe la base: ");
+//            base = pedirNumero();
+//
+//            System.out.println("Escribe el exponente");
+//            exponente = pedirNumero();
+//            
+//            resultado = Math.pow(base, exponente);
+//            System.out.println("El resultado es " + resultado);
+//            
+//        } catch(InputMismatchException e) {
+//            System.out.println("Error: tipo de dato incorrecto");
+//        } catch(ArithmeticException e) {
+//            System.out.println("No puedes elevar 0 a un número negativo");
+//        }
+                
+    }
+}
