973e7d34c590be34a5db2741cbe53e150cbadd24
d3stroya <anabel.comino@gmail.com>
2025-02-07 15:56:32 +0100

    Tema 7 - String[] args

---
+++ SaludoDesdeMain/src/saludodesdemain/SaludoDesdeMain.java
@@ -0,0 +1,20 @@
+
+package saludodesdemain;
+
+
+
+/**
+ *
+ * @author d3stroya
+ */
+public class SaludoDesdeMain {
+
+    /**
+     * Método main con el menú del programa
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola, " + args[0]);
+    }
+
+}
